76cebab9a903aafe2978232229e77c5ca3b7d3f4
Kostis Sagonas <kostis@cs.ntua.gr>
2010-10-01 21:40:47 +0800

    Avoid unnecessary construction of deep strings
    Especially when the -spec specifies that a string() is returned.

--- src/getopt.erl
+++ src/getopt.erl
@@ -456,10 +456,10 @@ usage_options_reverse([{Name, Short, Long, _ArgSpec, Help} | Tail], Acc) ->
                 case Short of
                     % Only long form.
                     undefined ->
-                        [$-, $-, Long];
+                        [$-, $- | Long];
                     % Both short and long form.
                     _ ->
-                        [$-, Short, $,, $\s, $-, $-, Long]
+                        [$-, Short, $,, $\s, $-, $- | Long]
                 end
         end,
     usage_options_reverse(Tail, add_option_help(Prefix, Help, Acc));
